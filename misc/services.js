45dd955c93cd64065eed9699e8cca9f31a3652f4
Alexander Verbruggen <alex@nabu.be>
2017-08-30 15:32:37 +0200

    Updated

--- misc/services.js
+++ misc/services.js
@@ -7,6 +7,7 @@ if (!nabu.services) { nabu.services = {}; }
 nabu.services.ServiceManager = function() {
 	var self = this;
 	this.$definitions = [];
+	
 	for (var i = 0; i < arguments.length; i++) {
 		this.$definitions.push(arguments[i]);
 	}
@@ -15,57 +16,85 @@ nabu.services.ServiceManager = function() {
 		return this.$register(this.$definitions);
 	}
 	
-	this.$register = function(services) {
+	this.$register = function(services, target) {
+		if (!target) {
+			target = self;
+		}
 		if (!(services instanceof Array)) {
 			services = [services];
 		}
+		
 		var promises = [];
 		
 		var initializeSingle = function(instance, name) {
-			var result = instance.$initialize();
+			var result = instance.$initialize ? instance.$initialize() : null;
 			if (result) {
 				// we assume a promise
 				if (result.then) {
 					result.then(function(service) {
 						if (service && name) {
-							self[name] = service;
+							service.$initialized = new Date();
+							target[name] = service;
 						}
 					});
 					promises.push(result);
 				}
 				// we assume that you returned the actual service instance
 				else if (name) {
-					self[name] = result;
+					target[name] = result;
 				}
+			}
+			else {
+				target[name] = instance;
 			}
 		};
 		
 		for (var i = 0; i < services.length; i++) {
-			var instance = new services[i](self);
-			var name = services[i].name 
-				? services[i].name.substring(0, 1).toLowerCase() + services[i].name.substring(1) 
-				: null;
-			if (name) {
-				self[name] = instance;
+			if (services[i] instanceof Function) {
+				var instance = new services[i](self);
+				var name = services[i].name 
+					? services[i].name.substring(0, 1).toLowerCase() + services[i].name.substring(1) 
+					: null;
+				if (instance.$initialize) {
+					initializeSingle(instance, name);	
+				}
 			}
 			else {
-				console.warn("Unnamed service", services[i]);
-			}
-			if (instance.$initialize) {
-				initializeSingle(instance, name);	
+				var names = Object.keys(services[i]);
+				for (var j = 0; j < names.length; j++) {
+					var name = names[j].substring(0, 1).toLowerCase() + names[j].substring(1);
+					if (services[i][names[j]] instanceof Function) {
+						var instance = new services[i][names[j]](self);
+						instance.$initialized = new Date();
+						initializeSingle(instance, name);
+					}
+					else {
+						target[name] = {};
+						promises.push(this.$register([services[i][names[j]]], target[name]));
+					}
+				}
 			}
 		}
 		return new nabu.utils.promises(promises);
 	}
 	
-	this.$clear = function() {
+	this.$clear = function(target) {
+		if (!target) {
+			target = self;
+		}
 		var promises = [];
-		for (var key in self) {
-			if (key.substring(0, 1) != "$" && self[key].$clear) {
-				var result = self[key].$clear();
-				if (result && result.then) {
-					promises.push(result);
+		for (var key in target) {
+			if (target[key].$initialized) {
+				if (key.substring(0, 1) != "$" && target[key].$clear) {
+					var result = target[key].$clear();
+					target[key].$initialized = new Date();
+					if (result && result.then) {
+						promises.push(result);
+					}
 				}
+			}
+			else if (target[key] instanceof Object) {
+				this.$clear(target[key]);
 			}
 		}
 		return new nabu.utils.promises(promises);
